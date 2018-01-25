cad64f2afa1160525b9478bb268edf83aa212a2f
Rohan Saigal <rohan4saigal@gmail.com>
2018-01-24 19:28:07 -0500

    add submit button

--- src/MainFocus.js
+++ src/MainFocus.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import { Input, Header } from "semantic-ui-react";
+import { Input, Header, Button, Icon } from "semantic-ui-react";
 
 class MainFocus extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      mainFocus: ''
+    }
+  }
+
+  handleChange(event) {
+    //set state based on what user types in
+    this.setState({mainFocus: event.target.value})
   }
 
   render() {
     return (
       <div>
         <Header size="large">
-          Which of your main obsession's will you tackle today
+          Which of your main obsessions will you tackle today?
         </Header>
-        <Input placeholder="Today\'s Main Focus..." />
+        <Input placeholder="Today's Main Focus..." />
+        <Button animated>
+          <Button.Content visible><Icon name='right arrow' /></Button.Content>
+          <Button.Content hidden>Carpe Diem!</Button.Content>
+        </Button>
       </div>
     );
   }
 }
 
-export default MainFocus
+export default MainFocus;
