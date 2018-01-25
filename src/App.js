09a40c6673d4dac006eb1113b79492094fb35b9b
Rohan Saigal <rohan4saigal@gmail.com>
2018-01-24 20:02:32 -0500

    err: form is up but unable to type text

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class App extends Component {
     <div className="App">
         <Menu pointing secondary>
           <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+          <Menu.Item name='priorities' active={activeItem === 'priorities'} onClick={this.handleItemClick} />
+          <Menu.Item name='appointments' active={activeItem === 'appointments'} onClick={this.handleItemClick} />
           <Menu.Menu position='right'>
             <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
           </Menu.Menu>
--- src/MainFocus.js
+++ src/MainFocus.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Input, Header, Button, Icon } from "semantic-ui-react";
+import { Input, Header, Button, Icon, Form, TextArea } from "semantic-ui-react";
 
 class MainFocus extends Component {
   constructor(props) {
@@ -7,25 +7,36 @@ class MainFocus extends Component {
     this.state = {
       mainFocus: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
     //set state based on what user types in
     this.setState({mainFocus: event.target.value})
   }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({mainFocus: ''})
+  }
 
   render() {
     return (
-      <div>
+      <Form onSubmit={e => this.handleSubmit(e)}>
+
         <Header size="large">
           Which of your main obsessions will you tackle today?
         </Header>
-        <Input placeholder="Today's Main Focus..." />
-        <Button animated>
+        <TextArea
+          rows={1}
+          placeholder="Today's Main Focus..."
+          value={this.state.mainFocus}
+          onChange={e => this.handleSubmit(e)} />
+        <Button type="submit" animated>
           <Button.Content visible><Icon name='right arrow' /></Button.Content>
           <Button.Content hidden>Carpe Diem!</Button.Content>
         </Button>
-      </div>
+      </Form>
     );
   }
 }
