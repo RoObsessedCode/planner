a006e7bb499c963daf50484aacd4557125d25906
Rohan Saigal <rohan4saigal@gmail.com>
2018-01-26 15:37:21 -0500

    horizontally align owl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 //import { withRouter, Link } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import { Menu, Image } from "semantic-ui-react";
 import logo from "./logo.svg";
 
-import MainFocus from './MainFocus'
+import MainFocus from "./MainFocus";
 import "./App.css";
 
 class App extends Component {
@@ -14,24 +14,41 @@ class App extends Component {
     };
   }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name})
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
     return (
-    <div className="App">
+      <div className="App">
         <Menu pointing secondary>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='priorities' active={activeItem === 'priorities'} onClick={this.handleItemClick} />
-          <Menu.Item name='appointments' active={activeItem === 'appointments'} onClick={this.handleItemClick} />
-          <Menu.Menu position='right'>
-            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="priorities"
+            active={activeItem === "priorities"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="appointments"
+            active={activeItem === "appointments"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position="right">
+            <Menu.Item
+              name="logout"
+              active={activeItem === "logout"}
+              onClick={this.handleItemClick}
+            />
           </Menu.Menu>
         </Menu>
+        <div className="App-OwlDiv" >
+          <Image className="App-OwlImg" src={require("./owl.ico")} size="small" />
+        </div>
         <MainFocus />
-
       </div>
-
     );
   }
 }
